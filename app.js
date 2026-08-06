410e36717cde79e65027adf866334c619bc0c44e
HajikhanovKh <khagani49@gmail.com>
2026-08-06 22:52:12 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2131,7 +2131,7 @@ document.addEventListener("DOMContentLoaded", function() {
             payload.push({
                 idarereisivezifesi: raportAyarlarData.idarereisivezifesi || "",
                 idarereisi: raportAyarlarData.idarereisi || "",
-                mesulsexsvezifeyeri: raportAyarlarData.mesulsexsvezife || raportAyarlarData.mesulsexsvezifesi || "",
+                mesulsexsvezifeyeri: raportAyarlarData.mesulsexsvezife ||  "tapilmadi",
                 mesulsexs: raportAyarlarData.mesulsexs || "",
                 raportfirma: item.firmaAdi,
                 uzanti: item.isFiziki ? "na" : "nin",
