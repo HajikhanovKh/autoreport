49ee10e4d58333f5ce056290e06c0de5501ee127
HajikhanovKh <khagani49@gmail.com>
2026-08-06 23:03:12 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2131,7 +2131,8 @@ document.addEventListener("DOMContentLoaded", function() {
             payload.push({
                 idarereisivezifesi: raportAyarlarData.idarereisivezifesi || "",
                 idarereisi: raportAyarlarData.idarereisi || "",
-                mesulsexsvezifeyeri: [raportAyarlarData.mesulsexsvezife, raportAyarlarData.mesulsexsvezifesi].find(val => val && val !== "undefined") || "tapilmadi",
+                // Əvvəlki kodunuzu silib, yalnız bu şəkildə yazın:
+mesulsexsvezifeyeri: "MÜFƏTTİŞ TEST DİAQNOSTİKA",
                 mesulsexs: raportAyarlarData.mesulsexs || "",
                 raportfirma: item.firmaAdi,
                 uzanti: item.isFiziki ? "na" : "nin",
